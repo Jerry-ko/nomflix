d02f90f199f6a5174594aa909964d8e21d180986
Minju Ko <70134713+Jerry-ko@users.noreply.github.com>
2021-09-21 16:25:22 +0900

    #2.2 React Router part Two

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,11 @@
 import React, {Component} from "react"
 import Router from "Components/Router"
+import Header from "Components/Header"
 
 function App() {
   return (
     <>
+    <Header />
     <Router />
     </>
   );
