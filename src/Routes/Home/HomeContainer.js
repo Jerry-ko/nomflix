792706eedfa6869f1ac15c9d3ddadb870441ad13
Minju Ko <70134713+Jerry-ko@users.noreply.github.com>
2021-09-28 12:48:29 +0900

    #5.2 Home Container

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React from "react"
 import HomePresenter from "./HomePresenter"
+import { movieApi } from "api"
 
 export default class extends React.Component {
     state = {
@@ -8,6 +9,33 @@ export default class extends React.Component {
         popular: null,
         error: null,
         loading: true
+    }
+
+    async componentDidMount() {
+        try {
+            const {
+                data: {results : nowPlaying}
+            } = await movieApi.nowPlaying();
+            const {
+                data: {results: upcoming}
+            } = await movieApi.upcoming();
+            const {
+                data: {results: popular}
+            } = await movieApi.popular()
+            this.setState ({
+                nowPlaying,
+                upcoming,
+                popular
+            })   
+        } catch {
+            this.setState({
+                error: "Can't find movies infomation."
+            })
+        } finally {
+            this.setState({
+                loading: false
+            })
+        }
     }
 
     render() {
