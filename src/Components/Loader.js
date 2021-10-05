8cc422e6a4ca211f862dc75a07e75f4b0dbf7b64
Minju Ko <70134713+Jerry-ko@users.noreply.github.com>
2021-10-05 10:48:45 +0900

    #6.4 Message Component

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -12,7 +12,7 @@ const Container = styled.div`
 
 export default () => (
     <Container>
-        <span role="img" aria-lavbel="Loading">
+        <span role="img" aria-label="Loading">
             ⏰
         </span>
     </Container>
